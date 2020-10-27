48b207c3a70d0aa5406961566f8b6c85ca87e940
qinlili23333 <yeyaohua2010@163.com>
2020-10-27 22:33:26 +0800

    Initial

---
+++ iresearchDownloader/Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(1, 367)
+        Me.ProgressBar1.Margin = New System.Windows.Forms.Padding(4)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(1067, 41)
+        Me.ProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous
+        Me.ProgressBar1.TabIndex = 1
+        '
+        'TextBox1
+        '
+        Me.TextBox1.AllowDrop = True
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.TextBox1.Font = New System.Drawing.Font("等线 Light", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(163, 172)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(904, 39)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoEllipsis = True
+        Me.Label1.Font = New System.Drawing.Font("等线", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(2, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(896, 160)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "请把试读页面的链接丢进来,比如https://www.iresearchbook.cn/f/cdf/read?bookId=3bef82c795754015b08" &
+    "932ba713b6243&cdf=/201907291513471541/3bef82c795754015b08932ba713b6243.cdf&range" &
+    "=14&type=1&TRI=0"
+        Me.Label1.UseMnemonic = False
+        '
+        'TextBox2
+        '
+        Me.TextBox2.AllowDrop = True
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox2.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.TextBox2.Font = New System.Drawing.Font("等线", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(163, 217)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(904, 39)
+        Me.TextBox2.TabIndex = 4
+        Me.TextBox2.Text = "NewDownload.pdf"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("等线", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(6, 172)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(143, 33)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "页面链接"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("等线", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(6, 217)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(111, 33)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "文件名"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("等线", 16.125!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(0, 282)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(250, 79)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "内建下载器"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("等线", 16.125!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(256, 282)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(224, 79)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "CURL下载"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("等线", 16.125!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(486, 282)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(293, 79)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "复制下载链接"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoEllipsis = True
+        Me.Label4.Font = New System.Drawing.Font("等线", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(2, 413)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(1065, 170)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "内建下载器为VB.NET自带下载模块,单线程,兼容性好,默认使用系统代理.CURL下载会调用系统CURL,需求Win10,单线程,默认不使用代理.复制得到的下载链" &
+    "接不能直接在浏览器内打开,需要使用部分下载工具下载.本工具只能获取下载链接，不能绕过校园IP限制，请在支持使用爱学术的校园网下使用本工具！"
+        Me.Label4.UseMnemonic = False
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("等线", 16.125!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(785, 282)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(282, 79)
+        Me.Button4.TabIndex = 11
+        Me.Button4.Text = "开源Repo"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("等线", 16.125!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(889, 9)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(178, 144)
+        Me.Button5.TabIndex = 12
+        Me.Button5.Text = "爱学术"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(192.0!, 192.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.ClientSize = New System.Drawing.Size(1067, 579)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.ImeMode = System.Windows.Forms.ImeMode.Disable
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "爱学术PDF下载器V1.0.0@琴梨梨"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Public WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+End Class
---
+++ iresearchDownloader/Form1.vb
@@ -0,0 +1,56 @@
+﻿Imports System.Net
+Imports System.Web
+
+Public Class Form1
+    Dim DownloadClient As New WebClient
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        AddHandler DownloadClient.DownloadProgressChanged, AddressOf ShowDownProgress
+        AddHandler DownloadClient.DownloadFileCompleted, AddressOf DownloadFileCompleted
+    End Sub
+
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+    End Sub
+    Private Sub Form1_Click(sender As Object, e As EventArgs) Handles Me.Click
+
+    End Sub
+    Private Sub ShowDownProgress(ByVal sender As Object, ByVal e As DownloadProgressChangedEventArgs)
+        Invoke(New Action(Of Integer)(Sub(i) ProgressBar1.Value = i), e.ProgressPercentage)
+    End Sub
+    Private Sub DownloadFileCompleted(ByVal sender As Object, ByVal e As System.ComponentModel.AsyncCompletedEventArgs)
+        MessageBox.Show("下载成功!")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim pdfQuery = HttpUtility.ParseQueryString(New Uri(TextBox1.Text).Query)
+        Dim bookId = pdfQuery.GetValues("bookId")(0)
+        Dim cdf = pdfQuery.GetValues("cdf")(0)
+        Dim downloadURL = "https://www.iresearchbook.cn/f/cdf/file?file=" + cdf + "&bookId=" + bookId
+        DownloadClient.DownloadFileAsync(New Uri(downloadURL), (TextBox2.Text))
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim pdfQuery = HttpUtility.ParseQueryString(New Uri(TextBox1.Text).Query)
+        Dim bookId = pdfQuery.GetValues("bookId")(0)
+        Dim cdf = pdfQuery.GetValues("cdf")(0)
+        Dim downloadURL = "https://www.iresearchbook.cn/f/cdf/file?file=" + cdf + "&bookId=" + bookId
+        Shell("curl -o " + TextBox2.Text + " " + downloadURL)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim pdfQuery = HttpUtility.ParseQueryString(New Uri(TextBox1.Text).Query)
+        Dim bookId = pdfQuery.GetValues("bookId")(0)
+        Dim cdf = pdfQuery.GetValues("cdf")(0)
+        Dim downloadURL = "https://www.iresearchbook.cn/f/cdf/file?file=" + cdf + "&bookId=" + bookId
+        Clipboard.SetText(downloadURL)
+        MessageBox.Show("复制成功！")
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        System.Diagnostics.Process.Start("https://github.com/qinlili23333/iresearchDownloader")
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        System.Diagnostics.Process.Start("https://www.iresearchbook.cn/")
+    End Sub
+End Class
+
